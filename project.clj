2aaf7b24b0f6fee58f2acde2e53cb97549871581
matke994 <matke994@gmail.com>
2018-08-08 23:51:21 +0200

    2

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (defproject clojure_project_webapp "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -15,21 +14,3 @@
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
-=======
-(defproject clojure_project_webapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.0-alpha5"]
-                 [mysql/mysql-connector-java "5.1.6"]
-                 [de.ubercode.clostache/clostache "1.4.0"]
-                 [ring-basic-authentication "1.0.5"]]
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler clojure-project-webapp.core/-main}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
->>>>>>> 1
--- src/clojure_project_webapp/core.clj
+++ src/clojure_project_webapp/core.clj
@@ -6,12 +6,60 @@
             [ring.middleware.basic-authentication :refer :all]
             [ring.util.response :as resp]
             [clojure-project-webapp.domain.file :as file-domain]
-            [clojure-project-webapp.domain.transaction :as transaction-domain])
+            [clojure-project-webapp.domain.transaction :as transaction-domain]
+            [clojure-project-webapp.webController.controller :as controller]))
 
 
-  )
+(defroutes public-routes
+           (GET "/" [] (controller/index))
+           (route/resources "/")
+           (GET "/index" [] (controller/index))
+           (route/resources "/")
+           (GET "/homepage" [] (controller/home))
+           (route/resources "/")
+           (GET "/allTransactions" [] (controller/allTransactions))
+           (route/resources "/")
+           (GET "/allFiles" [] (controller/allFiles))
+           (route/resources "/")
 
-(defn -main
-  "I don't do a whole lot...."
-  [x]
-  (println x "Hello, World"))
+           (GET "/domain/transactions/:id/remove" [id]
+             (do (transaction-domain/removeTransaction id)
+                 (resp/redirect "/allTransactions")))
+
+           (GET "/domain/files/:id/remove" [id]
+             (do (file-domain/removeFile id)
+                 (resp/redirect "/allFiles")))
+
+           (POST "/domain/transactions/insert" [& params]
+             (do (transaction-domain/insertTransaction params)
+                 (resp/redirect "/allTransactions")))
+
+           (POST "/domain/files/insert" [& params]
+             (do (file-domain/insertFile params)
+                 (resp/redirect "/allFiles")))
+
+           (GET "/domain/transactions/:id/update" [id]
+             (controller/updatingTransaction id))
+
+           (POST "/domain/transactions/:TXN_ID/update" [& params]
+             (do (transaction-domain/update (:TXN_ID params) params)
+                 (resp/redirect "/allTransactions")))
+
+           (GET "/domain/files/:id/update" [id]
+             (controller/updatingFile id))
+
+           (POST "/domain/files/:FILE_ID/update" [& params]
+             (do (file-domain/update (:FILE_ID params) params)
+                 (resp/redirect "/allFiles")))
+
+           )
+
+
+
+
+(defroutes app-routes
+           public-routes
+           (route/not-found "404. Page not found"))
+
+(def -main
+  (handler/site app-routes))
